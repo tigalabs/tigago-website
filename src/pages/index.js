c9ce682e9193ee6b7b6d54e999aead1717982071
misitebao <i@misitebao.com>
2021-05-27 22:24:06 +0800

    perf: 更新网站标题和描述

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,8 +38,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="A modular framework based on Go language">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
